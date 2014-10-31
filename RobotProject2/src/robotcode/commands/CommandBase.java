071d70bd42a198d997c064f1414e8581ee2771dc
18akhila <18akhila@students.harker.org>
2014-10-30 18:02:16 -0700

    What I have done so far

--- RobotProject2/src/robotcode/commands/CommandBase.java
+++ RobotProject2/src/robotcode/commands/CommandBase.java
@@ -3,19 +3,20 @@ package robotcode.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robotcode.OI;
+import robotcode.subsystems.Drivetrain;
 import robotcode.subsystems.ExampleSubsystem;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
  * CommandBase stores creates and stores each control system. To access a
  * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
- * @author Author
+ * @author Akhil Arun
  */
 public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
-    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static Drivetrain driveTrain = new Drivetrain();
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
@@ -26,7 +27,7 @@ public abstract class CommandBase extends Command {
         oi = new OI();
 
         // Show what command your subsystem is running on the SmartDashboard
-        SmartDashboard.putData(exampleSubsystem);
+        
     }
 
     public CommandBase(String name) {
---
+++ RobotProject2/src/robotcode/subsystems/Drivetrain.java
@@ -0,0 +1,78 @@
+package robotcode.subsystems;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ *
+ * @author 18AkhilA
+ */
+public class Drivetrain extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    Talon talon1 = new Talon(1);// Initializes each Talon with what each port
+    Talon talon2 = new Talon(2);// it is located in
+    Talon talon3 = new Talon(3);
+    Talon talon4 = new Talon(4);
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+  
+    /**
+     * Makes the robot move forward at full speed
+     */
+    public void forward()
+    {
+        
+        talon1.set(1);
+        talon2.set(1);
+        talon3.set(1);
+        talon4.set(1);
+        
+    }
+    
+    /**
+     * Makes the robot go backwards at full speed
+     */
+    public void reverse()
+    {
+        talon1.set(-1);
+        talon2.set(-1);
+        talon3.set(-1);
+        talon4.set(-1);
+        
+    }
+    
+    /**
+     * Sets the speed to a desired speed
+     * @param speed is the speed that the talons will be set as
+     */
+    public void setSpeed(double speed)
+    {
+        talon1.set(speed);
+        talon2.set(speed);
+        talon3.set(speed);
+        talon4.set(speed);
+    }
+    
+    /**
+     * This stops all of the Talons
+     */
+    public void stop()
+    {
+        talon1.set(0);
+        talon2.set(0);
+        talon3.set(0);
+        talon4.set(0);
+        
+    }
+    
+}
